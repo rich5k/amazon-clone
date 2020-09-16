1d076b0b99948a15319702768eafc1b6c84aefcd
rich5k <richardanatsui@gmail.com>
2020-09-17 00:08:33 +0100

    added the product details to view more component

--- src/ViewMore.js
+++ src/ViewMore.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import "./ViewMore.css"
-function ViewMore() {
+function ViewMore({id,title,image,price,rating}) {
+    const[{basket}, dispatch]= useStateValue();
+   
+    const addToBasket=()=>{
+        //dispatch the item into the data layer
+        dispatch({
+            type:'ADD_TO_BASKET',
+            item:{
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            }
+        })
+    }
     return (
         <div className="viewMore">
-            
+            <img src={image} />
+            <div className='product__info'>
+                <p>{title}</p>
+            </div>
+            <div className='product__rating'>
+                {Array(rating).fill().map((_, i)=>(
+                    <p><StarIcon /></p>
+
+                ))}
+            </div>
+            <div className='product__price'>
+                <small>$</small>
+                <strong>{price}</strong>
+            </div>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
